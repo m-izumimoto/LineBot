1bcd7c0aa1b20437e69947074ddf5f9c7a3e68f6
izumimoto <izumimoto@interpark.co.jp>
2018-10-19 11:51:21 +0900

    gazou3

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ foreach ((array)$events as $event){
   //オウム返し
   // $bot->replyText($event->getReplyToken(), $event->getText());
   // 画像を返信する
-  replyImageMessage($bot, $event->getReplyToken(),'https://' . $_SERVER[HTTP_HOST] . '/imgs/cat.jpg' , 'https://' . $_SERVER[HTTP_HOST] . '/imgs/dog.jpg');
+  replyImageMessage($bot, $event->getReplyToken(),'https://' . $_SERVER['HTTP_HOST'] . '/imgs/cat.jpg' , 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/dog.jpg');
 }
 
 //テキストを返信。引数はLINEBot、返信先、テキスト
